71025fbe852633a8a412ca02d561fb53f0cf23fc
Heroicos_HM <houghtonawe@gmail.com>
2020-07-31 19:08:39 -0700

    initial commit

---
+++ DiscordBot/src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+// Console.WriteLine()
+
+namespace DiscordBot 
+{
+    public class Program
+    {
+        public static Task Main(string[] args)
+            => Startup.RunAsync(args);
+    }
+}
---
+++ DiscordBot/src/Services/Logging.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace DiscordBot
+{
+    public class LoggingService
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _commands;
+        
+        private string _logDirectory { get;  }
+        private string _logFile => Path.Combine(_logDirectory, $"{DateTime.UtcNow.ToString("yyyy-MM-dd")}.txt");
+
+        public LoggingService(DiscordSocketClient client, CommandService commands)
+        {
+            _logDirectory = Path.Combine(AppContext.BaseDirectory, "logs");
+
+            _client = client;
+            _commands = commands;
+
+            _client.Log += OnLogAsync;
+            _commands.Log += OnLogAsync;
+        }
+
+        private Task OnLogAsync(LogMessage msg)
+        {
+            if (!Directory.Exists(_logDirectory))
+                Directory.CreateDirectory(_logDirectory);
+            if (!File.Exists(_logFile))
+                File.Create(_logFile).Dispose();
+
+            string logText =
+                $"{DateTime.UtcNow.ToString("hh:mm:ss")} [{msg.Severity}] {msg.Source}: {msg.Exception?.ToString() ?? msg.Message}";
+
+            return Console.Out.WriteLineAsync(logText);
+        }
+    }
+}
